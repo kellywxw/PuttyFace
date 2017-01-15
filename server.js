26b06e3ffaaa17b262fd97fb5e4ea7dd6d9148fa
Kelly Wang <kellywxw@ccs.neu.edu>
2017-01-14 21:11:37 -0500

    puttyface

---
+++ server.js
@@ -0,0 +1,47 @@
+var bodyParser = require('body-parser');
+var multer = require('multer');
+var mongoose = require('mongoose');
+var passport = require('passport');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var express = require('express');
+var moment = require('moment-timezone')
+var app = express();
+
+// create a default connection string
+var connectionString = 'mongodb://localhost/FormBuilderDB';
+
+// use remote connection string
+// if running in remote server
+if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
+    connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+        process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+        process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+        process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+        process.env.OPENSHIFT_APP_NAME;
+}
+
+var db = mongoose.connect(connectionString);
+
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+app.use(multer());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true
+}));
+app.use(cookieParser());
+app.use(passport.initialize());
+app.use(passport.session());
+
+var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+
+/*
+var chopchopUserModel = require("./public/project/server/models/user.model.server.js")(db, mongoose);
+require("./public/assignment/server/app.js")(app, mongoose, db, chopchopUserModel);
+require("./public/project/server/app.js")(app, mongoose, db, chopchopUserModel);
+*/
+app.listen(port, ipaddress);
